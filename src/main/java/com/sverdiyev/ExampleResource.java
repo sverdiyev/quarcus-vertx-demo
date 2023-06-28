2ee0b295ea004fb9bffa1d6f8dac97db599cf992
sverdiyev <oleksandr.verdiyev@devbridge.com>
2023-06-28 18:23:19 +0200

    add post endpoint

--- src/main/java/com/sverdiyev/ExampleResource.java
+++ src/main/java/com/sverdiyev/ExampleResource.java
@@ -1,10 +1,12 @@
 package com.sverdiyev;
 
 import com.sverdiyev.models.Fruit;
+import io.quarkus.hibernate.reactive.panache.Panache;
 import io.quarkus.panache.common.Sort;
 import io.smallrye.mutiny.Uni;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.GET;
+import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
@@ -13,7 +15,7 @@ import java.util.List;
 @Produces(MediaType.APPLICATION_JSON) // default
 @Consumes(MediaType.APPLICATION_JSON) // default
 //@ApplicationScoped // why is this needed?
-@Path("/elo")
+@Path("/")
 public class ExampleResource {
 
   @GET
@@ -30,14 +32,30 @@ public class ExampleResource {
 
   @GET
   @Path("/fruits")
-  public Uni<List<Fruit>> getAllUsers() {
+  public Uni<List<Fruit>> getAllFruits() {
     return Fruit.listAll(Sort.by("id"));
   }
 
   @GET
-  @Path("/specific-fruit")
+  @Path("/fruits/{name}")
   @Produces(MediaType.APPLICATION_JSON)
-  public Uni<Fruit> getAllUsers3() {
-    return Fruit.findByName("s");
+  public Uni<Fruit> getFruit(String name) {
+    return Fruit.findByName(name);
+  }
+
+  @POST
+  @Path("/fruits")
+  public void createFruit(Fruit fruit) { //is not reactive - does not work. does not save fruit
+
+    Panache.withTransaction(fruit::persist).onItem()
+      .invoke(() -> System.out.println("Saved"));
+  }
+
+  @POST
+  @Path("/fruits2")
+  public Uni<Void> createFruit2(Fruit fruit) { //reactive - this works. Saves fruit
+
+    return Panache.withTransaction(fruit::persist).onItem()
+      .invoke(() -> System.out.println("Saved")).replaceWithVoid();
   }
 }
