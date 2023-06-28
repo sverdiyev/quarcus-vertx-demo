343c2acf0172ea94915c7c497fd70b6ab2d98c69
sverdiyev <oleksandr.verdiyev@devbridge.com>
2023-06-28 21:15:41 +0200

    cleanup

--- src/main/java/com/sverdiyev/vertx/PeriodicFruitFetcher.java
+++ src/main/java/com/sverdiyev/vertx/PeriodicFruitFetcher.java
@@ -5,24 +5,12 @@ import io.smallrye.mutiny.Uni;
 import io.smallrye.mutiny.vertx.core.AbstractVerticle;
 import io.vertx.mutiny.core.eventbus.EventBus;
 import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.inject.Inject;
 import java.time.Duration;
-import org.eclipse.microprofile.context.ManagedExecutor;
 
 @SuppressWarnings("java:S106")
 @ApplicationScoped // is that smth like @Component? So it is available in other parts of the app?
 public class PeriodicFruitFetcher extends AbstractVerticle {
 
-  //  public static final String ADDRESS = "showMeTheTime_address";
-  @Inject
-  ManagedExecutor executor;
-
-//  @ConsumeEvent(ADDRESS)
-////  @ConsumeEvent(value = ADDRESS)
-//  private Uni<String> consumer(String someText) {
-//
-//    return Uni.createFrom().item("ITS PARTY TIME" + someText);
-//  }
 
   @Override
   public Uni<Void> asyncStart() {
@@ -30,24 +18,18 @@ public class PeriodicFruitFetcher extends AbstractVerticle {
 
     vertx.setPeriodic(Duration.ofSeconds(1).toMillis(), item -> {
 
-      //sending a message and awaiting response ASYNC
-//      bus.<String>request(ADDRESS, "hello there").onItem()
-//        .invoke(response -> System.out.println(response));
-
       bus.<String>request("greeting", "sasha")
         .onItem().invoke(response -> System.out.println(response));
+
+      bus.<String>request("greeting2", "sasha")
+        .onItem().invoke(response -> System.out.println(response));
+
 
     });
     System.out.println("hello there from vertx verticle");
     return Uni.createFrom().voidItem();
   }
 
-//  @ConsumeEvent(ADDRESS)
-//  private String consumerBlocking(String someText) {
-//
-//    System.out.println("do we receive it? ");
-//    return "ITS PARTY TIME" + someText;
-//  }
 
   @ConsumeEvent("greeting")
   public String consume(String name) {
@@ -56,7 +38,7 @@ public class PeriodicFruitFetcher extends AbstractVerticle {
 
   @ConsumeEvent("greeting2")
   public Uni<String> consume2(String name) {
-    return Uni.createFrom().item(() -> name.toUpperCase()).emitOn(executor);
+    return Uni.createFrom().item(name::toUpperCase);
   }
 
 }
