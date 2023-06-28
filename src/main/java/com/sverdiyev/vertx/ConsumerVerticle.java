c2034c52abeb07b027ecfe3a523e6c74527430ea
sverdiyev <oleksandr.verdiyev@devbridge.com>
2023-06-28 21:27:33 +0200

    split consumer and provider

---
+++ src/main/java/com/sverdiyev/vertx/ConsumerVerticle.java
@@ -0,0 +1,29 @@
+package com.sverdiyev.vertx;
+
+import io.quarkus.vertx.ConsumeEvent;
+import io.smallrye.mutiny.Uni;
+import io.smallrye.mutiny.vertx.core.AbstractVerticle;
+import jakarta.enterprise.context.ApplicationScoped;
+
+@SuppressWarnings("java:S106")
+@ApplicationScoped
+public class ConsumerVerticle extends AbstractVerticle {
+
+  @Override
+  public Uni<Void> asyncStart() {
+    System.out.println("consumer deployed");
+    return Uni.createFrom().voidItem();
+  }
+
+  @ConsumeEvent("greeting")
+  public String consume(String name) {
+    System.out.println("consumer 1 runs");
+    return name.toUpperCase();
+  }
+
+  @ConsumeEvent("greeting2")
+  public Uni<String> consume2(String name) {
+    System.out.println("consumer 2 runs");
+    return Uni.createFrom().item(name::toUpperCase);
+  }
+}
--- src/main/java/com/sverdiyev/vertx/PeriodicFruitFetcher.java
+++ src/main/java/com/sverdiyev/vertx/PeriodicFruitFetcher.java
@@ -1,6 +1,5 @@
 package com.sverdiyev.vertx;
 
-import io.quarkus.vertx.ConsumeEvent;
 import io.smallrye.mutiny.Uni;
 import io.smallrye.mutiny.vertx.core.AbstractVerticle;
 import io.vertx.mutiny.core.eventbus.EventBus;
@@ -11,34 +10,21 @@ import java.time.Duration;
 @ApplicationScoped // is that smth like @Component? So it is available in other parts of the app?
 public class PeriodicFruitFetcher extends AbstractVerticle {
 
-
   @Override
   public Uni<Void> asyncStart() {
     final EventBus bus = vertx.eventBus();
 
+    //greeting 1
     vertx.setPeriodic(Duration.ofSeconds(1).toMillis(), item -> {
-
       bus.<String>request("greeting", "sasha")
-        .onItem().invoke(response -> System.out.println(response));
+        .onItem().invoke(response -> System.out.println(response.body()));
 
+      //greeting 2
       bus.<String>request("greeting2", "sasha")
-        .onItem().invoke(response -> System.out.println(response));
-
-
+        .onItem().invoke(response -> System.out.println(response.body()));
     });
+
     System.out.println("hello there from vertx verticle");
     return Uni.createFrom().voidItem();
   }
-
-
-  @ConsumeEvent("greeting")
-  public String consume(String name) {
-    return name.toUpperCase();
-  }
-
-  @ConsumeEvent("greeting2")
-  public Uni<String> consume2(String name) {
-    return Uni.createFrom().item(name::toUpperCase);
-  }
-
 }
